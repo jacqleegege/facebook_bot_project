b2193b8765a9be3aaa3c2b374100b7db910f123d
Jacqueline Lee <jacqleegege@gmail.com>
2021-07-07 12:16:24 +0700

    Completed bot

---
+++ message.js
@@ -0,0 +1,93 @@
+const puppeteer = require("puppeteer");
+const CREDS = require('./creds')
+const prompt = require('prompt-sync')();
+const ObjectsToCsv = require('objects-to-csv')
+const fs = require('fs');
+const readline = require('readline');
+const csv = require('csv-parser');
+const neatCsv = require('neat-csv');
+
+const USERNAME_SELECTOR = '#email';
+const PASSWORD_SELECTOR = '#pass';
+const SUBMIT_SELECTOR = '[type="submit"]';
+const SEARCH_SELECTOR = '[type="search"]';
+const MESSAGE_BTN_SELECTOR = "//div[contains(text(),'Send Message')]";
+const COMMUNITY_FILTER = '&filters=eyJjYXRlZ29yeTowIjoie1wibmFtZVwiOlwicGFnZXNfY2F0ZWdvcnlcIixcImFyZ3NcIjpcIjI2MTJcIn0ifQ%3D%3D';
+const TEXTBOX_SELECTOR = 'div.notranslate._5rpu';
+const CLOSE_CHAT_BTN_SELECTOR = '[aria-label="Close chat"][role="button"]';
+const OK_BTN_SELECTOR = '[aria-label="OK"][role="button"]';
+
+function message() {
+
+  return new Promise(async (resolve, reject) => {
+  try {
+
+    console.log('Messages will be sent to communities listed in comsList.csv!');
+    console.log('Note : If comsLIst.csv is not created yet, please exit this code by ctrl-C, and type node start');
+    const wait = prompt('Click on any button to continue! If comsList.csv is not finalized yet, now is your chance :D');
+
+
+    const MESSAGE_TO_SEND = prompt("Enter message to send : ");
+    console.log(`Message to send is ${MESSAGE_TO_SEND}`);
+
+    var browser =  await puppeteer.launch({headless:false});
+    var page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(1000000);
+    await page.setViewport({ width: 1000, height: 600 });
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions("https://www.facebook.com", ['notifications']);
+    await page.goto("https://www.facebook.com");
+
+
+    //logging in
+    console.log('Logging in..');
+    await page.waitForSelector(USERNAME_SELECTOR);
+    await page.type(USERNAME_SELECTOR, CREDS.username);
+    await page.type(PASSWORD_SELECTOR, CREDS.password);
+    await page.click(SUBMIT_SELECTOR);
+    await page.waitForTimeout(2000);
+    await page.waitForNavigation();
+
+    console.log('Starting messaging process to communities listed in comsList.csv!');
+
+    fs.readFile('./comsList.csv', async (err, data) => {
+      if (err) {
+        console.error(err);
+        return
+      }
+      const communitiesToMessage = await neatCsv(data);
+      for (var i = 0; i < communitiesToMessage.length; i++) {
+        await page.goto(communitiesToMessage[i].url);
+
+
+        //click on message button
+        // await page.click(MESSAGE_BTN_SELECTOR);
+        await page.evaluate((MESSAGE_BTN_SELECTOR) => {
+          var messageElement = document.evaluate(MESSAGE_BTN_SELECTOR, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+          messageElement.click();
+        }, MESSAGE_BTN_SELECTOR);
+
+        await page.waitForSelector(TEXTBOX_SELECTOR);
+        await page.type(TEXTBOX_SELECTOR, MESSAGE_TO_SEND);
+
+        // the dangerous 'send button'
+        // await (await page.$(TEXTBOX_SELECTOR)).press('\n');
+
+        await page.click(CLOSE_CHAT_BTN_SELECTOR);
+        await page.waitForSelector(OK_BTN_SELECTOR);
+        await page.click(OK_BTN_SELECTOR);
+      }
+      console.log("Messaging process completed! Have a nice day :)")
+      console.log("Closing browser..")
+      await browser.close();
+    })
+
+
+
+  } catch(e) {
+      return reject(e);
+  }
+  })
+}
+
+message().then(console.log).catch(console.error);
--- start.js
+++ start.js
@@ -7,44 +7,26 @@ const readline = require('readline');
 const csv = require('csv-parser');
 const neatCsv = require('neat-csv');
 
-
-const UNI_NAME = 'Kings College London';
 const USERNAME_SELECTOR = '#email';
 const PASSWORD_SELECTOR = '#pass';
 const SUBMIT_SELECTOR = '[type="submit"]';
 const SEARCH_SELECTOR = '[type="search"]';
-const MESSAGE_BTN_SELECTOR = '[aria-label="Message"][role="button"]';
+const MESSAGE_BTN_SELECTOR = "//div[contains(text(),'Send Message')]";
 const COMMUNITY_FILTER = '&filters=eyJjYXRlZ29yeTowIjoie1wibmFtZVwiOlwicGFnZXNfY2F0ZWdvcnlcIixcImFyZ3NcIjpcIjI2MTJcIn0ifQ%3D%3D';
-const PATH_TO_LINK_VIA_PAGES = 'div[role="article"]>div>div>div>div>div>div:nth-child(2)>div>div>h2>span>span>span>a';
 const TEXTBOX_SELECTOR = 'div.notranslate._5rpu';
 const CLOSE_CHAT_BTN_SELECTOR = '[aria-label="Close chat"][role="button"]';
 const OK_BTN_SELECTOR = '[aria-label="OK"][role="button"]';
 const FOLLOWERS_SELECTOR = 'div.taijpn5t.cbu4d94t.j83agx80 > div > div > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.cbu4d94t.g5gj957u.d2edcug0.hpfvmrgz.rj1gh0hx.buofh1pr.o8rfisnq.p8fzw8mz.pcp91wgn.iuny7tx3.ipjc6fyt > div > div > span > span';
 
-function login() {
-  //todo: implement user input for username and password
-  return new Promise(async (resolve, reject) => {
-    try {
-
-
-       //logging in
-       await page.waitForSelector(USERNAME_SELECTOR);
-       await page.type(USERNAME_SELECTOR, CREDS.username);
-       await page.type(PASSWORD_SELECTOR, CREDS.password);
-       await page.click(SUBMIT_SELECTOR);
-       await page.waitForTimeout(600);
-       await page.waitForNavigation();
-     } catch(e) {
-       throw "Login failed! Please enter correct username or password"
-   }
- })
-}
 
 function run() {
     // dom element selectors for login page
     const MESSAGE_TO_SEND = prompt("Enter message to send : ");
-    console.log(`Message to send is ${MESSAGE_TO_SEND}`);
+    console.log(`Message to send is ${MESSAGE_TO_SEND}.`);
+    console.log("Note: you'll need an input file listing each university. Please make sure that all the words are capitalized, and one uni per line!");
     const INPUT_FILE = prompt("Enter file name (please make sure that file is in the folder!) : ");
+    console.log(`Input file is ${INPUT_FILE}.`);
+    console.log("Note: if there hasn't been any response from the terminal, that means something is wrong! Please ctrl-C and restart!")
 
 
     return new Promise(async (resolve, reject) => {
@@ -60,6 +42,7 @@ function run() {
 
 
     //logging in
+    console.log("Logging in..");
     await page.waitForSelector(USERNAME_SELECTOR);
     await page.type(USERNAME_SELECTOR, CREDS.username);
     await page.type(PASSWORD_SELECTOR, CREDS.password);
@@ -80,6 +63,7 @@ function run() {
     //initial array to store data of communities searched
     const comsData = [];
 
+    console.log("Starting to fill up comsList.csv!");
     for await (const UNI_NAME of rl) {
       // Each line in input.txt will be successively available here as `line`.
 
@@ -124,39 +108,38 @@ function run() {
         let follows = document.querySelector(FOLLOWERS_SELECTOR).textContent;
         return follows;
       }, FOLLOWERS_SELECTOR);
-      console.log(followers);
       comsData.push({
         name: communities[i].text,
         followers: followers,
         url: communities[i].url,
       });
-
     }
 
   }
+  console.log(comsData);
 
   //output a csv file for comsData
   const csv = new ObjectsToCsv(comsData);
   await csv.toDisk('./comsList.csv', { append: true });
-  console.log(comsData);
+  console.log('comsList.csv has been filled(or produced if not already)');
 
     //todo: figure out a way to take coms as input
   const continuing = prompt("Would you like to continue? (Y/N): ");
   if (continuing.toString().trim() === 'N') {
     console.log("You can always type 'node message' if you want to immediately start messaging! :D");
-    console.log ("Closing process..");
+    console.log ("Closing process, have a great day!!");
     browser.close();
     return;
   }
-  const change = prompt("Would you like to make changes to the message list? (Y/N):");
+  const change = prompt("Would you like to make changes to the message list? (Y/N): ");
   if (change.toString().trim() === 'Y') {
+    console.log("Note: when editing comsList.csv, please do not leave any empty line or space in between, or I'll crash!")
     const changed = prompt("Please make your changes now to comsList.csv and click any button to continue");
   }
 
   console.log("Messaging all the pages in file comsList.csv...");
 
-  const fr = require('fs');
-  fr.readFile('./comsList.csv', async (err, data) => {
+  fs.readFile('./comsList.csv', async (err, data) => {
     if (err) {
       console.error(err)
       return
@@ -164,14 +147,16 @@ function run() {
     const communitiesToMessage = await neatCsv(data);
     for (var i = 0; i < communitiesToMessage.length; i++) {
       await page.goto(communitiesToMessage[i].url);
-      await page.waitForNavigation();
+
 
       //click on message button
-      //FIX TO MAKE SURE THIS WORKS ON ALL CASES
-      await page.click(MESSAGE_BTN_SELECTOR);
+      // await page.click(MESSAGE_BTN_SELECTOR);
+      await page.evaluate((MESSAGE_BTN_SELECTOR) => {
+        var messageElement = document.evaluate(MESSAGE_BTN_SELECTOR, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        messageElement.click();
+    }, MESSAGE_BTN_SELECTOR)
 
-
-      console.log(communities)
+      await page.waitForSelector(TEXTBOX_SELECTOR);
       await page.type(TEXTBOX_SELECTOR, MESSAGE_TO_SEND);
 
       // the dangerous 'send button'
@@ -181,96 +166,11 @@ function run() {
       await page.waitForSelector(OK_BTN_SELECTOR);
       await page.click(OK_BTN_SELECTOR);
     }
+
+    console.log("Messaging process completed! Have a nice day :)")
     await browser.close();
   })
 
-  // fr.createReadStream('./comsList.csv')
-  // .pipe(csv())
-  // .on('data', async function (row) {
-  //   page.goto(row.url);
-  //   await page.waitForNavigation();
-  //
-  //   //click on message button
-  //   await page.click(MESSAGE_BTN_SELECTOR);
-  //
-  //   await page.waitForSelector(TEXTBOX_SELECTOR);
-  //
-  //   await page.type(TEXTBOX_SELECTOR, MESSAGE_TO_SEND);
-  //
-  //   // the dangerous 'send button'
-  //   // await (await page.$(TEXTBOX_SELECTOR)).press('\n');
-  //
-  //   await page.click(CLOSE_CHAT_BTN_SELECTOR);
-  //   await page.waitForSelector(OK_BTN_SELECTOR);
-  //   await page.click(OK_BTN_SELECTOR);
-  // })
-  // .on('end', async function () {
-  //   })
-  // console.log(communitiesToMessage.length);
-  // for (var i = 0; i < communitiesToMessage.length; i++) {
-  //   page.goto(communitiesToMessage.url);
-  //   await page.waitForNavigation();
-  //
-  //   //click on message button
-  //   await page.click(MESSAGE_BTN_SELECTOR);
-  //
-  //   await page.waitForSelector(TEXTBOX_SELECTOR);
-  //
-  //   await page.type(TEXTBOX_SELECTOR, MESSAGE_TO_SEND);
-  //
-  //   // the dangerous 'send button'
-  //   // await (await page.$(TEXTBOX_SELECTOR)).press('\n');
-  //
-  //   await page.click(CLOSE_CHAT_BTN_SELECTOR);
-  //   await page.waitForSelector(OK_BTN_SELECTOR);
-  //   await page.click(OK_BTN_SELECTOR);
-  // }
-
-
-
-
-    // start messaging process
-    // for (var i = 0; i < communities.length; i++) {
-    //   page.goto(communities[i].url);
-    //   await page.waitForNavigation();
-    //
-    //   //click on message button
-    //   await page.click(MESSAGE_BTN_SELECTOR);
-    //
-    //   await page.waitForSelector(TEXTBOX_SELECTOR);
-    //
-    //   await page.type(TEXTBOX_SELECTOR, MESSAGE_TO_SEND);
-    //   // await (await page.$(TEXTBOX_SELECTOR)).press('\n');
-    //
-    //   await page.click(CLOSE_CHAT_BTN_SELECTOR);
-    //   await page.waitForSelector(OK_BTN_SELECTOR);
-    //   await page.click(OK_BTN_SELECTOR);
-    //   }
-  //
-  // let firstCom = await page.evaluate(() => {
-  //   let item = document.querySelector('div[role="article"]>div>div>div>div>div>div:nth-child(2)>div>div>h2>span>span>span>a');
-  //   let url = item.getAttribute('href');
-  //   let text = item.textContent;
-  //   return url;
-  // })
-  // page.goto(firstCom);
-  // await page.waitForNavigation();
-  //
-  // //click on message button
-  // await page.click(MESSAGE_BTN_SELECTOR);
-  //
-  // await page.waitForSelector(TEXTBOX_SELECTOR);
-  //
-  // await page.type(TEXTBOX_SELECTOR, MESSAGE_TO_SEND);
-  // await (await page.$(TEXTBOX_SELECTOR)).press('\n');
-  //
-  // await page.click('[aria-label="Close chat"][role="button"]');
-  // await page.waitForSelector('[aria-label="OK"][role="button"]');
-  // await page.click('[aria-label="OK"][role="button"]');
-
-
-  //browser.close();
-  return resolve();
 
   } catch(e) {
   return reject(e);
