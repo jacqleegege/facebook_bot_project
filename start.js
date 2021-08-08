847eda8f63de52575a3f9d1051fb90c2ad21eb81
Jacqueline Lee <jacqleegege@gmail.com>
2021-08-08 21:40:49 +0700

    Added feature to search for types of societies

--- start.js
+++ start.js
@@ -16,7 +16,7 @@ const TEXTBOX_SELECTOR = 'div.notranslate._5rpu';
 const CLOSE_CHAT_BTN_SELECTOR = '[aria-label="Close chat"][role="button"]';
 const OK_BTN_SELECTOR = '[aria-label="OK"][role="button"]';
 const FOLLOWERS_SELECTOR = 'div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.cbu4d94t.g5gj957u.d2edcug0.hpfvmrgz.rj1gh0hx.buofh1pr.o8rfisnq.p8fzw8mz.pcp91wgn.iuny7tx3.ipjc6fyt > div > div > span > span';
-
+const COOKIES_SELECTOR = 'div._9o-r button:nth-child(2)';
 
 function run() {
     // dom element selectors for login page
@@ -24,11 +24,17 @@ function run() {
     console.log(`Message to send is ${MESSAGE_TO_SEND}.`);
     console.log("Would you like to send message now?");
     const SEND_MESSAGE = prompt("(No is for testing in case you wanna watch bot in action!!) (Y/N) ");
-    console.log("Note: you'll need an input file listing each university. Please make sure that all the words are capitalized, and one uni per line!");
-    const INPUT_FILE = prompt("Enter file name (please make sure that file is in the folder!) : ");
-    console.log(`Input file is ${INPUT_FILE}.`);
+    console.log("Note: you'll need 2 files listing each university and societies respectively. Please make sure that all the words are capitalized, and one uni per line!");
+    console.log("Additionally, please make sure that they are all in the same folder!");
+    const UNI_NAME_FILE = prompt("Enter file name for list of universities : ");
+    console.log(`Input file is ${UNI_NAME_FILE}.`);
+    const SOC_NAME_FILE = prompt("Enter file name for types of societies : ");
+    console.log(`Input file is ${SOC_NAME_FILE}.`);
     console.log("Note: if there hasn't been any response from the terminal, that means something is wrong! Please ctrl-C and restart!")
 
+    var num_searches = 3;
+    console.log("Please choose whether to produce all search results or only the first 3.");
+    const searches = prompt("Would you like to add all search results? (Y/N) : ")
 
     return new Promise(async (resolve, reject) => {
     try {
@@ -42,8 +48,8 @@ function run() {
     await page.goto("https://www.facebook.com");
 
     //cookie problem
-    await page.waitForSelector('div._9o-r button:nth-child(2)');
-    await page.click('div._9o-r button:nth-child(2)');
+    await page.waitForSelector(COOKIES_SELECTOR);
+    await page.click(COOKIES_SELECTOR);
 
     //logging in
     console.log("Logging in..");
@@ -54,8 +60,21 @@ function run() {
     await page.waitForTimeout(2000);
     await page.waitForNavigation();
 
+    //array to store types of society to search for
+    const socTypes = [];
 
-    const fileStream = fs.createReadStream(INPUT_FILE);
+    const fileStream2 = fs.createReadStream(SOC_NAME_FILE);
+
+    const rl2 = readline.createInterface({
+      input: fileStream2,
+      crlfDelay: Infinity
+    });
+
+    for await (const SOC_NAME of rl2) {
+      socTypes.push(SOC_NAME);
+    }
+
+    const fileStream = fs.createReadStream(UNI_NAME_FILE);
 
     const rl = readline.createInterface({
       input: fileStream,
@@ -63,65 +82,69 @@ function run() {
     });
     // Note: we use the crlfDelay option to recognize all instances of CR LF
     // ('\r\n') in input.txt as a single line break.
-
     //initial array to store data of communities searched
     const comsData = [];
 
     console.log("Starting to fill up comsList.csv!");
     for await (const UNI_NAME of rl) {
-      // Each line in input.txt will be successively available here as `line`.
+      // Each line in input.txt will be successively available here as `UNI_NAME`.
+      for await (const SOC_NAME of socTypes) {
 
-    //search uni name
-    await page.waitForSelector(SEARCH_SELECTOR);
-    await page.click(SEARCH_SELECTOR);
-    await page.type(SEARCH_SELECTOR, UNI_NAME);
-    await page.waitForTimeout(600);
+      //search uni name
+      var society = UNI_NAME.concat(" ", SOC_NAME);
+      await page.waitForSelector(SEARCH_SELECTOR);
+      await page.click(SEARCH_SELECTOR);
+      await page.type(SEARCH_SELECTOR, society);
+      await page.waitForTimeout(600);
 
-    await (await page.$(SEARCH_SELECTOR)).press('\n');
+      await (await page.$(SEARCH_SELECTOR)).press('\n');
 
-    //filter to pages of category 'community'
-    //Pre: uni entered must be full caps not sure why though urgh
-    await page.waitForNavigation();
-    var pages = page.url().replace("top", "pages").concat(COMMUNITY_FILTER);
-    page.goto(pages);
-
-    //go to profile of first result
-    await page.waitForNavigation();
-    await page.waitForTimeout(600);
-
-    //produces result of the search
-    //TODO: improve to include everything when scroll down
-    let communities = await page.evaluate(() => {
-      let results = [];
-      let items = document.querySelectorAll('div[role="article"]>div>div>div>div>div>div:nth-child(2)>div>div>h2>span>span>span>a');
-      items.forEach((item) => {
-        results.push({
-          url:  item.getAttribute('href'),
-          text: item.textContent,
-        });
-      });
-      // results.shift();
-
-      return results;
-    })
-
-    //comsData contains name, followers, and url for each community
-    for (var i = 0; i < communities.length; i++) {
-      page.goto(communities[i].url);
+      //filter to pages of category 'community'
+      //Pre: uni entered must be full caps not sure why though urgh
       await page.waitForNavigation();
-      await page.waitForSelector(FOLLOWERS_SELECTOR);
-      let likes = await page.evaluate((FOLLOWERS_SELECTOR) => {
-        let follows = document.querySelector(FOLLOWERS_SELECTOR).textContent;
-        return follows;
-      }, FOLLOWERS_SELECTOR);
-      comsData.push({
-        name: communities[i].text,
-        likes: likes,
-        url: communities[i].url,
-      });
-      console.log(communities[i].text + " added");
-    }
+      var pages = page.url().replace("top", "pages").concat(COMMUNITY_FILTER);
+      page.goto(pages);
 
+      //go to profile of first result
+      await page.waitForNavigation();
+      await page.waitForTimeout(600);
+
+      //produces result of the search
+      let communities = await page.evaluate(() => {
+        let results = [];
+        let items = document.querySelectorAll('div[role="article"]>div>div>div>div>div>div:nth-child(2)>div>div>h2>span>span>span>a');
+        items.forEach((item) => {
+          results.push({
+            url:  item.getAttribute('href'),
+            text: item.textContent,
+          });
+        });
+
+        return results;
+      })
+
+
+      if (searches.toString().trim() === 'Y' || searches.toString().trim() === 'y') {
+        num_searches = communities.length;
+      }
+
+      //comsData contains name, followers, and url for each community
+      for (var i = 0; i < num_searches; i++) {
+        page.goto(communities[i].url);
+        await page.waitForNavigation();
+        await page.waitForSelector(FOLLOWERS_SELECTOR);
+        let likes = await page.evaluate((FOLLOWERS_SELECTOR) => {
+          let follows = document.querySelector(FOLLOWERS_SELECTOR).textContent;
+          return follows;
+        }, FOLLOWERS_SELECTOR);
+        comsData.push({
+          name: communities[i].text,
+          likes: likes,
+          url: communities[i].url,
+        });
+        console.log(communities[i].text + " added");
+      }
+    }
   }
   console.log(comsData);
 
